8ab618f86a2e4a599fe14851fd473ad2fe5e23d5
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-03-20 18:22:42 +0530

    initial commit

---
+++ Leetcode/prblm_11.sql
@@ -0,0 +1,9 @@
+-- 577. Employee Bonus
+-- Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+
+select e.name , b.bonus
+from Employee e
+left join Bonus b
+on e.empId = b.empID
+where b.bonus < 1000 or b.bonus is null;
+ 
