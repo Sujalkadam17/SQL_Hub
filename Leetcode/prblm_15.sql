172434e0d9d0b83eabd20c47fb8af1797b97037f
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-03-21 12:18:56 +0530

    initial upload

---
+++ Leetcode/prblm_15.sql
@@ -0,0 +1,8 @@
+-- 620. Not Boring Movies
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+-- Return the result table ordered by rating in descending order.
+
+select *
+from Cinema 
+where mod(id,2) = 1 and description != 'boring' 
+order by rating desc;
